b16dc08d2d22805428f53c4a640ad468db182e6d
xiatiansong <wuskyfantasy@gmail.com>
2015-12-21 18:32:22 +0800

    修改netty server处理

--- src/main/java/com/lumi/largedata/govsjava/netty/NettyHttpServer.java
+++ src/main/java/com/lumi/largedata/govsjava/netty/NettyHttpServer.java
@@ -1,44 +1,39 @@
 package com.lumi.largedata.govsjava.netty;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.http.HttpRequestDecoder;
-import io.netty.handler.codec.http.HttpResponseEncoder;
+
+import java.net.InetSocketAddress;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class NettyHttpServer {
 
 	private static Log log = LogFactory.getLog(NettyHttpServer.class);
 
 	public void start(int port) throws Exception {
-		EventLoopGroup bossGroup = new NioEventLoopGroup();
-		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		NioEventLoopGroup group = new NioEventLoopGroup();
 		try {
 			ServerBootstrap b = new ServerBootstrap();
-			b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+			b.group(group).channel(NioServerSocketChannel.class).localAddress(new InetSocketAddress(port))
 					.childHandler(new ChannelInitializer<SocketChannel>() {
 						@Override
 						public void initChannel(SocketChannel ch) throws Exception {
-							// server端发送的是httpResponse，所以要使用HttpResponseEncoder进行编码
-							ch.pipeline().addLast(new HttpResponseEncoder());
-							// server端接收到的是httpRequest，所以要使用HttpRequestDecoder进行解码
-							ch.pipeline().addLast(new HttpRequestDecoder());
 							ch.pipeline().addLast(new HttpServerInboundHandler());
 						}
-					}).option(ChannelOption.SO_BACKLOG, 128).childOption(ChannelOption.SO_KEEPALIVE, true);
-			ChannelFuture f = b.bind(port).sync();
+					});
+
+			ChannelFuture f = b.bind().sync();
+			System.out
+					.println(NettyHttpServer.class.getName() + " started and listen on " + f.channel().localAddress());
 			f.channel().closeFuture().sync();
 		} finally {
-			workerGroup.shutdownGracefully();
-			bossGroup.shutdownGracefully();
+			group.shutdownGracefully().sync();
 		}
 	}
 
